f62041c724ad140bc7a94832680a1bdab6a1f0be
Urji <urjiihd@gmail.com>
2025-05-05 19:18:33 +0300

    Added Solidity exercise codes

---
+++ Lesson 1/Exercise codes/Basic data types/Codebase/Unsigned integers.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    uint8 public a = 26;
+    uint16 public b = 2500;
+    uint256 public sum;
+
+    constructor() {
+        sum = a + b;
+    }
+}
